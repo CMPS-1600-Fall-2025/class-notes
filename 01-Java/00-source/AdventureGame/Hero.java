a91dedec6df6c1f225ff9b661d137de36cdebe07
Aaron Maus <arobie2@gmail.com>
2025-08-22 14:28:31 -0500

    Add javadoc comments

--- 01-Java/00-source/AdventureGame/Hero.java
+++ 01-Java/00-source/AdventureGame/Hero.java
@@ -1,8 +1,17 @@
+/**
+ * Class Hero represents the user protagonist character in our game.
+ */
 public class Hero {
     private String name;
     private int hp;
     private int damage;
 
+    /**
+     * 
+     * @param name the name for this hero
+     * @param hp the number of starting hit points for this hero
+     * @param damage the base amount of damage this hero deals in an attack
+     */
     public Hero(String name, int hp, int damage) {
         this.name = name;
         this.hp = hp;
