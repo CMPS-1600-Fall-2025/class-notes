58bee449301f4a4db482699ed0cbe050c721fdf3
Aaron Maus <arobie2@gmail.com>
2025-08-22 13:53:11 -0500

    Start Hero Class

---
+++ 01-Java/00-source/AdventureGame/Hero.java
@@ -0,0 +1,12 @@
+public class Hero {
+    private String name;
+    private int hp;
+    private int damage;
+
+    public Hero(String name, int hp, int damage) {
+        this.name = name;
+        this.hp = hp;
+        this.damage = damage;
+    }
+    
+}
